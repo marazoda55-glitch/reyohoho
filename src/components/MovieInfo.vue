9df2c219a6424e508723e32351a4e4883bf41c19
marazoda55-glitch <marazoda55@gmail.com>
2026-02-05 16:47:19 +0700

    Update MovieInfo.vue

--- src/components/MovieInfo.vue
+++ src/components/MovieInfo.vue
@@ -1,15 +1,32 @@
 <template>
   <div class="movie-info">
     <div class="content">
-       <PlayerComponent :id="$route.params.id" />
+      <div id="kinobox_player" style="width: 100%; height: 550px; background: #000; margin: 20px 0;"></div>
     </div>
   </div>
 </template>
 
 <script>
-import PlayerComponent from './PlayerComponent.vue';
 export default {
   name: 'MovieInfo',
-  components: { PlayerComponent }
+  mounted() {
+    // Удаляем старые скрипты, если они были
+    const oldScript = document.getElementById('kinobox-js');
+    if (oldScript) oldScript.remove();
+
+    const script = document.createElement('script');
+    script.id = 'kinobox-js';
+    script.src = 'https://kinobox.tv/static/kinobox.min.js';
+    script.async = true;
+    script.onload = () => {
+      if (window.Kinobox) {
+        new window.Kinobox('#kinobox_player', {
+          search: { kinopoisk: this.$route.params.id || location.href.split('/').pop() },
+          menu: { enable: true, default: 'Просмотр' }
+        }).init();
+      }
+    };
+    document.head.appendChild(script);
+  }
 }
 </script>
