edbed8368c39c77b976f3ebfe7cf4df1417765d1
marazoda55-glitch <marazoda55@gmail.com>
2026-02-05 16:43:52 +0700

    Create PlayerComponent.vue

---
+++ src/components/src/components/PlayerComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div style="width:100%; min-height:550px; background:#000; margin:20px 0;">
+    <div id="kinobox_player"></div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+onMounted(() => {
+  const s = document.createElement('script')
+  s.src = 'https://kinobox.tv/static/kinobox.min.js'
+  s.async = true
+  s.onload = () => {
+    if (window.Kinobox) {
+      new window.Kinobox('#kinobox_player', {
+        search: { kinopoisk: window.location.pathname.split('/').pop() }
+      }).init()
+    }
+  }
+  document.head.appendChild(s)
+})
+</script>
