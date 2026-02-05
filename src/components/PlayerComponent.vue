bc169b24252e09fa905c24e82b811b8fcf3b9fa7
marazoda55-glitch <marazoda55@gmail.com>
2026-02-05 16:31:13 +0700

    Update PlayerComponent.vue

--- src/components/PlayerComponent.vue
+++ src/components/PlayerComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="player-container">
+  <div class="player-box">
     <div id="kinobox_player" style="width: 100%; height: 550px; background: #000;"></div>
   </div>
 </template>
@@ -9,10 +9,10 @@ export default {
   name: 'PlayerComponent',
   props: ['id'],
   mounted() {
-    this.initKinobox();
+    this.loadKinobox();
   },
   methods: {
-    initKinobox() {
+    loadKinobox() {
       const script = document.createElement('script');
       script.src = 'https://kinobox.tv/static/kinobox.min.js';
       script.async = true;
@@ -20,7 +20,7 @@ export default {
         if (window.Kinobox) {
           new window.Kinobox('#kinobox_player', {
             search: { kinopoisk: this.id || location.href.split('/').pop() },
-            menu: { enable: true, default: 'Просмотр' }
+            menu: { enable: true, default: 'Выбор плеера' }
           }).init();
         }
       };
