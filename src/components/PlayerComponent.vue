af859e5729355c2f060624faec17a1c2aaae8dbd
marazoda55-glitch <marazoda55@gmail.com>
2026-02-05 16:25:12 +0700

    Update PlayerComponent.vue

--- src/components/PlayerComponent.vue
+++ src/components/PlayerComponent.vue
@@ -1,22 +1,31 @@
 <template>
-  <div id="kinobox_player" style="width: 100%; height: 550px; background: #000;"></div>
+  <div class="player-container">
+    <div id="kinobox_player" style="width: 100%; height: 550px; background: #000;"></div>
+  </div>
 </template>
 
 <script>
 export default {
+  name: 'PlayerComponent',
   props: ['id'],
   mounted() {
-    const script = document.createElement('script');
-    script.src = 'https://kinobox.tv/static/kinobox.min.js';
-    script.async = true;
-    script.onload = () => {
-      if (window.Kinobox) {
-        new window.Kinobox('#kinobox_player', {
-          search: { kinopoisk: this.id || location.href.split('/').pop() }
-        }).init();
-      }
-    };
-    document.head.appendChild(script);
+    this.initKinobox();
+  },
+  methods: {
+    initKinobox() {
+      const script = document.createElement('script');
+      script.src = 'https://kinobox.tv/static/kinobox.min.js';
+      script.async = true;
+      script.onload = () => {
+        if (window.Kinobox) {
+          new window.Kinobox('#kinobox_player', {
+            search: { kinopoisk: this.id || location.href.split('/').pop() },
+            menu: { enable: true, default: 'Просмотр' }
+          }).init();
+        }
+      };
+      document.head.appendChild(script);
+    }
   }
 }
 </script>
